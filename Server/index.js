c8c2c204ca0324fc707470baebd39c8ff01ebd71
CodeByDanial <70098547+danialisrar09@users.noreply.github.com>
2026-08-26 05:25:29 +0500

    Database Setup

--- Server/index.js
+++ Server/index.js
@@ -3,3 +3,15 @@ import express from "express";
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const App = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://Social-CRUD-App:vsQWrdR4qyTvDLCY@social-crud-app.7kzv47s.mongodb.net/?appName=Social-CRUD-App'
+
+
+
+//MongoDB Connection
+
