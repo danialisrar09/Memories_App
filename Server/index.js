84fc6f5c6aa48016a14df5830cf085e79fc5e097
CodeByDanial <70098547+danialisrar09@users.noreply.github.com>
2026-09-02 23:07:13 +0500

    DB Connection.

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,20 @@
 import bodyParser from "body-parser";
 import express from "express";
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 import cors from 'cors';
 
-const App = express();
+const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://Social-CRUD-App:vsQWrdR4qyTvDLCY@social-crud-app.7kzv47s.mongodb.net/?appName=Social-CRUD-App'
+const CONNECTION_URL = 'mongodb+srv://Danial:CodeByDanial@interview-ai-cluster.xa9sihf.mongodb.net/'
+const PORT = process.env.PORT || 5000;
 
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
 
 //MongoDB Connection
