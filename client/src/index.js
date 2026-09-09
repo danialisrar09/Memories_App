45c172d4ef030cb11b16dce959361cd17b2c8bb4
CodeByDanial <70098547+danialisrar09@users.noreply.github.com>
2026-09-09 20:44:06 +0500

    Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { createTheme, ThemeProvider } from '@mui/styles';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
+
+import reducers from "./reducers"
 
 import App from "./App";
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
